8a0d262873e877d0e4d21e9938c16b6c20122281
Dmytro Semeruk <deemoon83@gmail.com>
2021-10-25 15:58:13 +0300

    nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-function App() {
+import { Route } from "react-router-dom";
+import Container from "./Components/Container";
+import Navigation from "./Components/Navigation";
+import HomePage from "./Components/HomePage";
+import MoviesPage from "./Components/MoviesPage";
+
+export default function App() {
   return (
-    <div className="App">
-      <p>movie</p>
-    </div>
+    <Container>
+      <Navigation />
+      <Route exact path="/">
+        <HomePage />
+      </Route>
+      <Route path="/movies">
+        <MoviesPage />
+      </Route>
+    </Container>
   );
 }
-
-export default App;
---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom";
+import styles from "./Navigation.module.css";
+
+export default function Navigation() {
+  const { navigation, nav__link, nav__activeLink } = styles;
+  return (
+    <nav className={navigation}>
+      <NavLink
+        exact
+        to="/"
+        className={nav__link}
+        activeClassName={nav__activeLink}
+      >
+        Main
+      </NavLink>
+      <NavLink
+        to="/movies"
+        className={nav__link}
+        activeClassName={nav__activeLink}
+      >
+        Movies
+      </NavLink>
+      {/* <NavLink to="/movies/:movieId">Id</NavLink> */}
+      {/* <NavLink to="/movies/:movieId/cast">Cast</NavLink> */}
+      {/* <NavLink to="/movies/:movieId/reviews">Reviews</NavLink> */}
+    </nav>
+  );
+}
